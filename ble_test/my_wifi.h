2902af5e69c8628e5f0daa24548a85e55c6ab4da
heyj <heyj@starspulse.com>
2024-03-19 10:41:59 +0800

    加入esp32s3的灯光状态显示

--- ble_test/my_wifi.h
+++ ble_test/my_wifi.h
@@ -1,27 +1,26 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiAP.h>
-#include <vector>
-#include <esp_wifi.h>
 #include "Arduino.h"
-#define maxData 1024
-#define MyPort 1024
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WiFiClient.h>
+#include <esp_wifi.h>
+#include <vector>
+
+#define maxData       1024
+#define MyPort        1024
 #define minimum(a, b) (((a) < (b)) ? (a) : (b))
 
-extern int data_n ;
-extern int data_read_n ;
-extern int image_len ;
-extern int image_get_n ;
-extern int image_get_time ;
-extern int cmdtime ;
-extern  boolean send_img_flag;
-extern  boolean send_video_flag;
-
+extern int data_n;
+extern int data_read_n;
+extern int image_len;
+extern int image_get_n;
+extern int image_get_time;
+extern int cmdtime;
+extern boolean send_img_flag;
+extern boolean send_video_flag;
 
 void wifi_init();
 void getImage();
 void cmd_len();
 void cmd_data();
-extern  void (*ondonefunc)();
+extern void (*ondonefunc)();
 extern void (*timeoutfunc)();
-
