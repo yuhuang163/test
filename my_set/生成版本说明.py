bbd6b64ef47bcd501a4e0782c2c7312f8faaed78
heyj <heyj@starspulse.com>
2024-10-10 18:03:45 +0800

    提交记录

--- my_set/生成版本说明.py
+++ my_set/生成版本说明.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # 可配置的内容置顶,包括那天
 REPO_PATH = r"D:\new_production\new_production"
-SINCE_DATE = "2024-09-12"
+SINCE_DATE = "2024-10-9"
 UNTIL_DATE = "2024-12-31"
 
 # 定义头文件路径
